ca0426686dd7bc847e281578c15b4e0edbb10605
Hahahayabanan <olegvolobuev97@mail.ru>
2019-07-20 17:50:58 +0400

    Created rate-button, range-slider, added jquery-ui

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,13 @@ import $ from 'jquery'
 global.jQuery = $;
 global.$ = $;
 
+import '../blocks/jquery-ui/jquery-ui.min'
+
+
+
+
 import '../blocks/dropdown/dropdown-select/dropdown-select.js'
 import '../blocks/toggle-button/toggle-btn.js'
 import '../blocks/like-button/like-btn.js'
 
+import '../blocks/range-slider/range-slider.js'
