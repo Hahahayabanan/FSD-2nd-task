8aef672ebd64c2ec5ee01e6c0bf5b178661141f7
Hahahayabanan <olegvolobuev97@mail.ru>
2019-07-26 19:54:04 +0400

    Getting responsive

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -24,6 +24,8 @@ dropdown_guests_all.forEach(function(dropdown_guests,item){
     dropdown_guests.querySelector('.dropdown-select_guests').addEventListener('click', function() {
         dropdown_guests.classList.toggle('dropdown-select_active');
         dropdownOption_guests.style.visibility = (dropdownOption_guests.style.visibility == 'visible') ? 'hidden' : 'visible';
+        
+        if(guests_num.innerHTML > 0 || baby_num.innerHTML) option_button_clear.style.visibility = (option_button_clear.style.visibility == 'visible') ? 'hidden' : 'visible';
     });
 
 
