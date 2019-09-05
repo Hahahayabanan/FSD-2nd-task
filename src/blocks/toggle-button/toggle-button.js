d99e4d91fa94b38ccc6efcefef1cc7d33a404737
Hahahayabanan <olegvolobuev97@mail.ru>
2019-09-05 19:39:20 +0400

    Refactored toggle button

---
+++ src/blocks/toggle-button/toggle-button.js
@@ -0,0 +1,10 @@
+//$('input.cb-value').prop("checked", true);
+$('.toggle-button__cb').click(function() {
+    var mainParent = $(this).parent('.toggle-button');
+    if($(mainParent).find('input.toggle-button__cb').is(':checked')) {
+      $(mainParent).addClass('toggle-button_active');
+    } else {
+      $(mainParent).removeClass('toggle-button_active');
+    }
+  
+})
