5932aa0de4fef6d191d53629547d82acc5eb08ef
Hahahayabanan <olegvolobuev97@mail.ru>
2019-09-24 10:37:03 +0400

    fixed automatic page generating #2

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,8 +1,9 @@
-const path = require('path')
-const fs = require('fs')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const fs = require('fs');
+const async = require('async');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 // Main const
@@ -15,26 +16,41 @@ const PATHS = {
 // Pages const for HtmlWebpackPlugin
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
 const PAGES_DIR = `${PATHS.src}/pages/`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+
+function getFiles(dir, files_, folder = '') {
+  const filesArray = files_ || [];
+  let folderPath = folder;
+  fs.readdirSync(dir).map((filePath) => {
+    const name = path.join(dir, filePath);
+    if (fs.statSync(name).isDirectory()) {
+      folderPath = filePath;
+      getFiles(name, filesArray, folderPath);
+    } else if (filePath.endsWith('.pug')) {
+      filesArray.push(path.join(folderPath, filePath));
+    }
+  });
+  return filesArray;
+}
+const PAGES = getFiles(PAGES_DIR);
 
 const IMAGES = fs
   .readdirSync(`${PATHS.src}/blocks`)
   .filter((dirName) => fs.lstatSync(`${PATHS.src}/blocks/${dirName}`).isDirectory());
 
+
 module.exports = {
   // BASE config
   externals: {
-    paths: PATHS
+    paths: PATHS,
   },
   entry: {
     app: `${PATHS.src}/js`,
-    // module: `${PATHS.src}/your-module.js`,
   },
   output: {
     // filename: `js/[name].[hash].js`,
-    filename: `js/app.js`,
+    filename: 'js/app.js',
     path: PATHS.dist,
-    // publicPath: PATHS.dist
   },
   optimization: {
     splitChunks: {
@@ -43,41 +59,31 @@ module.exports = {
           name: 'vendors',
           test: /node_modules/,
           chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+          enforce: true,
+        },
+      },
+    },
   },
   module: {
     rules: [{
       test: /\.pug$/,
-      loader: 'pug-loader'
+      loader: 'pug-loader',
     }, {
       test: /\.js$/,
       loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, 
+      exclude: '/node_modules/',
+    },
     {
       test: /\.(eot|svg|ttf|woff|woff2)$/,
       use: {
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
           limit: 50000,
-          name: "./fonts/[name].[ext]", // Output below ./fonts
-          publicPath: "../", // Take the directory into account
+          name: './fonts/[name].[ext]', // Output below ./fonts
+          publicPath: '../', // Take the directory into account
         },
       },
     },
-    {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      exclude: /fonts/,
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'img/',
-      }
-    }, 
-
     {
       test: /\.scss$/,
       use: [
@@ -85,15 +91,15 @@ module.exports = {
         MiniCssExtractPlugin.loader,
         {
           loader: 'css-loader',
-          options: { sourceMap: true }
+          options: { sourceMap: true },
         }, {
           loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          options: { sourceMap: true, config: { path: './postcss.config.js' } },
         }, {
           loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ]
+          options: { sourceMap: true },
+        },
+      ],
     }, {
       test: /\.css$/,
       use: [
@@ -101,98 +107,36 @@ module.exports = {
         MiniCssExtractPlugin.loader,
         {
           loader: 'css-loader',
-          options: { sourceMap: true }
+          options: { sourceMap: true },
         }, {
           loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
-        }
-      ]
-    }
-  ]
+          options: { sourceMap: true, config: { path: './postcss.config.js' } },
+        },
+      ],
+    },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       // filename: "./css/[name].[hash].css"
-      filename: "./css/app.css"
+      filename: './css/app.css',
     }),
 
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
-      'window.jQuery': 'jquery'
+      'window.jQuery': 'jquery',
     }),
     new CopyWebpackPlugin([
       { context: `${PATHS.src}/img`, from: '**/*', to: './img' },
-      ...IMAGES.map((item) => {
-        return { context: `./src/blocks/${item}/img`, from: '**/*', to: './img' };
-      })
+      ...IMAGES.map((item) => ({ context: `./src/blocks/${item}/img`, from: '**/*', to: './img' })),
     ]),
 
-    // Automatic creation any html pages (Don't forget to RERUN dev server)
-    // see more: https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
-    // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
-    ...PAGES.map(page => new HtmlWebpackPlugin({
+    ...PAGES.map((page) => new HtmlWebpackPlugin({
       template: `${PAGES_DIR}/${page}`,
-      filename: `./${page.replace(/\.pug/,'.html')}`,
-      inject: false
+      filename: `./${page.replace(/.*[\\]/, '').replace(/\.pug/, '.html')}`,
+      inject: true,
     })),
 
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/index.pug`,
-      filename: './index.html',
-      inject: true
-    }),
-
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/ui-kit/colors-and-type.pug`,
-      filename: './colors-and-type.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/ui-kit/cards.pug`,
-      filename: './cards.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/ui-kit/form-elements.pug`,
-      filename: './form-elements.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/ui-kit/headers-and-footers.pug`,
-      filename: './headers-and-footers.html',
-      inject: true
-    }),
-
-
-    
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/website-pages/landing-page.pug`,
-      filename: './landing-page.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/website-pages/registration.pug`,
-      filename: './registration.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/website-pages/room-details.pug`,
-      filename: './room-details.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/website-pages/search-room.pug`,
-      filename: './search-room.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/website-pages/sign-in.pug`,
-      filename: './sign-in.html',
-      inject: true
-    }),
- 
-    
-    
   ],
 }
