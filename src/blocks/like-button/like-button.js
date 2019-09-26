a1ab34286e331395c3b29862c2906b06497c0823
Hahahayabanan <olegvolobuev97@mail.ru>
2019-09-26 11:51:09 +0400

    Fixed rich-checkbox, like-button

--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,30 +1,41 @@
 /* eslint-disable array-callback-return */
-const likeBtnList = document.querySelectorAll('.like-button');
 
-const likeBtn = Array.prototype.slice.call(likeBtnList);
-
-function likeBtnClick(event) {
-  element = event.target
-  element.classList.toggle('like-button_active');
-
-  const likeBtnHeart = element.querySelector('.like-button__heart');
-  const likeBtnLabel = element.querySelector('.like-button__label');
-  const materialIcon = likeBtnHeart.querySelector('.material-icons');
-
-  materialIcon.classList.toggle('material-icons_color_gray');
-  materialIcon.classList.toggle('material-icons_color_purple');
-  if (materialIcon.className.includes('material-icons_color_purple')) {
-    materialIcon.textContent = 'favorite'
-    likeBtnLabel.textContent = Number(likeBtnLabel.textContent) + Number(1);
-  } else {
-    materialIcon.textContent = 'favorite_border';
-    if (likeBtnLabel.textContent >= 0) {
-      likeBtnLabel.textContent = Number(likeBtnLabel.textContent) - Number(1);
-    }
+class LikeButton {
+  constructor(htmlElem) {
+    this.elem = htmlElem;
+    this.bindEventListeners();
   }
-  likeBtnLabel.classList.toggle('like-button__label_active');
+
+  bindEventListeners() {
+    this.elem.addEventListener('click', this.changeStage.bind(this));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  changeStage(event) {
+    element = this.elem;
+    element.classList.toggle('like-button_active');
+
+    const likeBtnHeart = element.querySelector('.like-button__heart');
+    const likeBtnLabel = element.querySelector('.like-button__label');
+    const materialIcon = likeBtnHeart.querySelector('.material-icons');
+
+    materialIcon.classList.toggle('material-icons_color_gray');
+    materialIcon.classList.toggle('material-icons_color_purple');
+    if (materialIcon.className.includes('material-icons_color_purple')) {
+      materialIcon.textContent = 'favorite'
+      likeBtnLabel.textContent = Number(likeBtnLabel.textContent) + Number(1);
+    } else {
+      materialIcon.textContent = 'favorite_border';
+      if (likeBtnLabel.textContent >= 0) {
+        likeBtnLabel.textContent = Number(likeBtnLabel.textContent) - Number(1);
+      }
+    }
+    likeBtnLabel.classList.toggle('like-button__label_active');
+  }
 }
 
-likeBtn.map((element) => {
-  element.addEventListener('click', likeBtnClick);
-});
+const likeBtnList = document.querySelectorAll('.like-button');
+
+if (likeBtnList) {
+  const likeBtnArray = Array.from(likeBtnList, (val) => new LikeButton(val));
+}
