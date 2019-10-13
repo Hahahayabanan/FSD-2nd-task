3a35a66559be3f3efb1968c8e71800a568e6634a
Hahahayabanan <olegvolobuev97@mail.ru>
2019-10-13 17:27:43 +0400

    Fixed one block - one mixin #26

--- src/blocks/dropdown-date/dropdown-date.js
+++ src/blocks/dropdown-date/dropdown-date.js
@@ -3,8 +3,8 @@ import DropdownCalendar from '../dropdown-calendar/dropdown-calendar.js';
 const dropdownCalendar = document.querySelectorAll('.js-dropdown-date');
 dropdownCalendar.forEach((val) => {
   const inputs = [];
-  inputs.push(val.querySelector('.dropdown-date__input-start-date > input'));
-  inputs.push(val.querySelector('.dropdown-date__input-end-date > input'));
+  inputs.push(val.querySelector('.dropdown-date-item__input-start-date > input'));
+  inputs.push(val.querySelector('.dropdown-date-item__input-end-date > input'));
   return new DropdownCalendar(val, inputs);
 });
 
