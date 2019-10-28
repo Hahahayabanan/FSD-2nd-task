88a6cc74a0cfb7d13967a5f5b01b41d14f7d311f
Hahahayabanan <olegvolobuev97@mail.ru>
2019-10-28 10:13:58 +0400

    Added custom class to plugin blocks #35

---
+++ src/blocks/date-picker-calendar/date-picker-calendar.js
@@ -0,0 +1,75 @@
+class DatePickerCalendar {
+  constructor($datepicker) {
+    this.$datepicker = $datepicker;
+
+    this.init();
+  }
+
+  init() {
+    this.findDOMElements();
+    this.initLanguage();
+    this.createApplyButton();
+    this.bindEventListeners();
+    this.removeCommaFromTitle();
+    this.replaceNavArrows();
+  }
+
+  findDOMElements() {
+    this.$datepickerHTML = this.$datepicker.$datepicker;
+    this.$calendarInput = this.$datepicker.$el;
+    this.$title = this.$datepickerHTML.find('.datepicker--nav-title');
+  }
+
+  setApplyButtonText(text) {
+    this.applyButtonText = text;
+  }
+
+  createApplyButton() {
+    this.$buttonsContainer = this.$datepickerHTML.find('.datepicker--buttons');
+    this.applyButton = document.createElement('a');
+    this.applyButton.textContent = this.applyButtonText;
+    this.applyButton.classList.add('datepicker__apply-button');
+    this.$buttonsContainer.append(this.applyButton);
+  }
+
+  bindEventListeners() {
+    this.applyButton.addEventListener('click', this.hideDatepicker.bind(this));
+  }
+
+  hideDatepicker() {
+    this.$datepicker.hide();
+  }
+
+  initLanguage() {
+    $.fn.datepicker.language.ru = {
+      days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
+      daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+      today: 'Сегодня',
+      clear: 'Очистить',
+      dateFormat: 'dd.mm.yyyy',
+      timeFormat: 'hh:ii',
+      firstDay: 1,
+    };
+    this.setApplyButtonText('Применить');
+  }
+
+  removeCommaFromTitle() {
+    this.$calendarInput.datepicker({
+      navTitles: {
+        days: 'MM <i>yyyy</i>',
+      },
+    });
+  }
+
+  replaceNavArrows() {
+    this.$calendarInput.datepicker({
+      nextHtml: '<i class="datepicker__material-icons datepicker__material-icons_color_purple">arrow_forward</i>',
+      prevHtml: '<i class="datepicker__material-icons datepicker__material-icons_color_purple">arrow_back</i>',
+    });
+  }
+}
+
+export default DatePickerCalendar;
