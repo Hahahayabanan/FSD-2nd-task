be4a286986cf3b797859822548e151b4ecd51720
Hahahayabanan <olegvolobuev97@mail.ru>
2019-10-31 17:43:28 +0400

    added dynamic attribute setting #39

--- src/blocks/dropdown-option/dropdown-option.js
+++ src/blocks/dropdown-option/dropdown-option.js
@@ -29,7 +29,7 @@ class DropdownOption {
     this.value = parseInt(this.number.textContent, 10);
     if (this.value === '') this.value = 0;
     if (this.value > 0) this.activateMinus();
-    this.name = this.option.querySelector('.js-dropdown-option__header > p.heading').textContent;
+    this.name = this.option.querySelector('.js-dropdown-option__header > .heading').textContent;
     if (this.option.hasAttribute('data-group')) {
       const { group } = this.option.dataset;
       if (group) {
