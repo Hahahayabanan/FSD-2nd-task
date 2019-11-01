7e32ffe4d4a103cd5cc74eaafebd3a07bb6f00d1
Hahahayabanan <olegvolobuev97@mail.ru>
2019-11-01 08:35:47 +0400

    Refactoring donut chart #36

--- src/blocks/donut-chart/donut-chart.js
+++ src/blocks/donut-chart/donut-chart.js
@@ -13,6 +13,7 @@ class DonutChart {
     this.setGradients();
     this.createPaths();
     this.setStartText();
+    this.setStartPath();
   }
 
   setStyleClass() {
@@ -34,67 +35,64 @@ class DonutChart {
     const width = 120;
     const height = 120;
     const thickness = 5;
+    const thicknessWithInner = thickness + 5;
     const radius = Math.min(width, height) / 2;
     return {
-      data, width, height, thickness, radius,
+      data, width, height, thickness, thicknessWithInner, radius,
     };
   }
 
   initDonutChart() {
     const {
-      data, width, height, thickness, radius,
+      data, width, height, thickness, thicknessWithInner, radius,
     } = this.getConstants();
 
-    const d3DonutChart = d3.select(this.donutChart)
+    this.d3DonutChart = d3.select(this.donutChart)
       .append('svg')
       .attr('class', this.styleClass.PIE)
       .attr('width', width)
       .attr('height', height);
-    this.svgObject = d3DonutChart.append('g')
+    this.svgObject = this.d3DonutChart.append('g')
       .attr('transform', `translate(${width / 2},${height / 2})`);
-    this.outerArc = d3.arc()
+    this.arcAttribute = d3.arc()
       .innerRadius(radius - thickness)
       .outerRadius(radius);
-    this.innerArc = d3.arc()
-      .innerRadius(radius - 10)
+    this.arcAttributeWithInner = d3.arc()
+      .innerRadius(radius - thicknessWithInner)
       .outerRadius(radius);
     this.pie = d3.pie()
       .value((d) => d.value)
       .sort(null)
       .padAngle(0.02);
-    this.defs = d3DonutChart.append('svg:defs');
+    this.defs = this.d3DonutChart.append('svg:defs');
     this.data = data;
   }
 
-  onPieClick(dataObject) {
+  handlePathsClick(dataObject, currentPath, paths) {
     const { data, index } = dataObject;
-
     this.valueTextField.style('fill', `url(#gradient${index})`)
       .text(`${data.value}`);
     this.descriptionTextField.style('fill', `url(#gradient${index})`)
       .text(`${data.name}`);
-  }
-
-  onPieClickColor(data, currentPath, paths) {
-    d3.selectAll('path')
-      .attr('d', this.outerArc);
-    d3.select(paths[currentPath])
-      .attr('d', this.innerArc);
+    this.paths.attr('d', this.arcAttribute);
+    d3.select(paths[currentPath]).attr('d', this.arcAttributeWithInner);
   }
 
   createPaths() {
-    this.svgObject.selectAll('path')
+    this.paths = this.svgObject.selectAll('path')
       .data(this.pie(this.data))
       .enter()
       .append('g')
-      .on('click', this.onPieClick.bind(this))
       .style('fill', (d, index) => `url(#gradient${index})`)
       .append('path')
-      .attr('d', this.outerArc)
+      .attr('d', this.arcAttribute)
       .attr('fill', (d, index) => `url(#gradient${index})`)
       .attr('class', this.styleClass.PATH)
-      .on('click', this.onPieClickColor.bind(this));
-    this.svgObject.select('path').attr('d', this.innerArc);
+      .on('click', this.handlePathsClick.bind(this));
+  }
+
+  setStartPath(valueNumber = 0) {
+    d3.select(this.paths.nodes()[valueNumber]).attr('d', this.arcAttributeWithInner);
   }
 
   setStartText(valueNumber = 0) {
@@ -106,16 +104,17 @@ class DonutChart {
       .style('fill', `url(#gradient${valueNumber})`);
     this.descriptionTextField = this.svgObject.append('text')
       .text('голосов')
-      .attr('class', this.styleClass.PIE)
+      .attr('class', this.styleClass.TEXT)
       .attr('text-anchor', 'middle')
       .attr('dy', '1.3em')
       .style('fill', `url(#gradient${valueNumber})`);
   }
 
   setGradients() {
-    this.purpleGradient = this.createGradient({ id: 0, startColor: '#BC9CFF', endColor: '#8BA4F9' });
-    this.greenGradient = this.createGradient({ id: 1, startColor: '#6FCF97', endColor: '#66D2EA' });
-    this.yellowGradient = this.createGradient({ id: 2, startColor: '#FFE39C', endColor: '#FFBA9C' });
+    this.gradients = [];
+    this.gradients.push(this.createGradient({ id: 0, startColor: '#BC9CFF', endColor: '#8BA4F9' }));
+    this.gradients.push(this.createGradient({ id: 1, startColor: '#6FCF97', endColor: '#66D2EA' }));
+    this.gradients.push(this.createGradient({ id: 2, startColor: '#FFE39C', endColor: '#FFBA9C' }));
   }
 
   createGradient(parameters) {
