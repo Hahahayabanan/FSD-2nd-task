6d9c82f32833fe0380d086206aa11c65e626580b
Hahahayabanan <olegvolobuev97@mail.ru>
2019-11-08 10:16:19 +0400

    Added getting element in text-field js class

--- src/blocks/date-text-field/date-text-field.js
+++ src/blocks/date-text-field/date-text-field.js
@@ -1,3 +1,5 @@
+import TextField from '../text-field/text-field';
+
 class DateTextField {
   constructor($container, type) {
     this.$container = $container;
@@ -8,7 +10,9 @@ class DateTextField {
   }
 
   setDOMElements() {
-    this.$dateTextField = this.$container.find(`.js-date-text-field__input-${this.type}-date > input`);
+    this.$dateTextFieldContainer = this.$container.find(`.js-date-text-field__input-${this.type}-date`);
+    this.textField = new TextField(this.$dateTextFieldContainer);
+    this.$dateTextField = this.textField.getElement();
   }
 
   get$Element() {
--- src/blocks/text-field/text-field.js
+++ src/blocks/text-field/text-field.js
@@ -1,18 +1,26 @@
 import 'inputmask/dist/jquery.inputmask.bundle';
 
 class TextField {
-  constructor(element) {
-    this.$textField = $(element);
-
-    this.init();
+  constructor(container) {
+    const isThatElement = $(container).hasClass('js-text-field');
+    if (isThatElement) this.$textField = $(container);
+    else this.$textField = $(container).find('input.js-text-field');
+    this.setMasks();
   }
 
-  init() {
-    this.$textField.inputmask({
-      alias: 'datetime',
-      inputFormat: 'dd.mm.yyyy',
-      placeholder: 'ДД.ММ.ГГГГ',
-    });
+  setMasks() {
+    const isMaskedTextField = this.$textField.hasClass('text-field_masked');
+    if (isMaskedTextField) {
+      this.$textField.inputmask({
+        alias: 'datetime',
+        inputFormat: 'dd.mm.yyyy',
+        placeholder: 'ДД.ММ.ГГГГ',
+      });
+    }
+  }
+
+  getElement() {
+    return this.$textField;
   }
 }
 
